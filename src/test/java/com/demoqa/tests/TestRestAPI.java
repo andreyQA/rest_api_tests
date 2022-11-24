ffa0d9297849371308a7479dac108bf13d20ff30
Andrey Izvekov <andrew_qa@pnblab.com>
2022-11-24 20:25:11 +0300

    5 API test

--- src/test/java/com/demoqa/tests/TestRestAPI.java
+++ src/test/java/com/demoqa/tests/TestRestAPI.java
@@ -1,16 +1,66 @@
 package com.demoqa.tests;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.*;
 
 public class TestRestAPI {
     @Test
-    void checkServerAnswer() {
+    void checkCreateUserWithoutParameters() {
         given()
                 .when()
-                .get("https://selenoid.autotests.cloud/status")
+                .post("https://reqres.in/api/users")
                 .then()
-                .statusCode(403);
+                .body("id", notNullValue());
     }
+    @Test
+    void checkCreateUserWithParameters() {
+        String data = "{\n" +
+                "    \"name\": \"username\",\n" +
+                "    \"job\": \"qa god\",\n" +
+                "    \"random\": \"blablabla\"\n" +
+                "}";
+        given()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .statusCode(201)
+                .body("id", not("null"),
+                        "name",notNullValue(),
+                        "job", notNullValue(),
+                        "random",notNullValue());
+    }
+    @Test
+    void checkCountUsersPerPage() {
+        given()
+                .when()
+                .get("https://reqres.in/api/users")
+                .then()
+                .statusCode(200)
+                .body("data.id", hasSize(Integer.parseInt("6")));
+    }
+    @Test
+    void checkNonexistentUserOnFirstPage() {
+        given()
+                .when()
+                .get("https://reqres.in/api/users")
+                .then()
+                .statusCode(200)
+                .body("data.email", not("blablauser@mail.com"));
+    }
+    @Test
+    void checkExistUserOnFirstPage() {
+        given()
+                .when()
+                .get("https://reqres.in/api/users")
+                .then()
+                .statusCode(200)
+                .body("data.email", hasItem("janet.weaver@reqres.in"));
+    }
+
 }
